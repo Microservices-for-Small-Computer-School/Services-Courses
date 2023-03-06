1297b373ca7b6d69e1b72620e5eba5cf5f4e48fa
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-06 12:40:17 +0530

    Updates

--- Source/Courses.API/Program.cs
+++ Source/Courses.API/Program.cs
@@ -1,6 +1,18 @@
+using Courses.API.Business;
+using Courses.API.Data.Dtos;
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet(HelloWorldRoutes.Root, () => "Hello Minimal API World from Root !!");
+
+app.MapGet(HelloWorldRoutes.HelloWorld, () =>
+{
+    return ApiResponseDto<string>.Create("Hello Minimal API World from /hw !!");
+});
+
+app.MapGet(HelloWorldRoutes.Api, DefaultResponseBusiness.SendDefaultApiEndpointOutput);
+
+app.MapGet(HelloWorldRoutes.ApiV1, () => DefaultResponseBusiness.SendDefaultApiEndpointV1Output());
 
 app.Run();
