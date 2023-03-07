08bbc333d5a41e1211a6325198e8bf96c5101fe7
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-07 09:48:40 +0530

    Swamy/05mar work (#4)
    * Create Services-Courses.sln
    
    * Initial version
    
    * Files
    
    * VS Code file
    
    * Other sessions Mark down
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Changes
    
    * Updates
    
    * Postman Collection
    
    * Postman Collection
    
    * Few Changes
    
    * Code Refactoring
    
    * Code Refactoring

--- Source/Courses.API/Program.cs
+++ Source/Courses.API/Program.cs
@@ -1,4 +1,5 @@
-using static Constants;
+using Courses.API.Business;
+using Courses.API.Data.Dtos;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
@@ -15,52 +16,3 @@ app.MapGet(HelloWorldRoutes.Api, DefaultResponseBusiness.SendDefaultApiEndpointO
 app.MapGet(HelloWorldRoutes.ApiV1, () => DefaultResponseBusiness.SendDefaultApiEndpointV1Output());
 
 app.Run();
-
-public static class Constants
-{
-    public static class HelloWorldRoutes
-    {
-        public static string Root => "/";
-
-        public static string HelloWorld => "/hw";
-
-        public static string Api => "/api";
-
-        public static string ApiV1 => "/api/v1";
-    }
-}
-
-public record ApiResponseDto<T>
-{
-    public bool Success { get; set; }
-
-    public string? Message { get; set; }
-
-    public DateTimeOffset DateRequested => DateTimeOffset.UtcNow;
-
-    public T? Data { get; set; }
-
-    public static ApiResponseDto<T> Create(T? data = default, string message = "Success", bool success = true)
-    {
-        return new()
-        {
-            Success = success,
-            Message = message,
-            Data = data
-        };
-    }
-}
-
-public static class DefaultResponseBusiness
-{
-    public static ApiResponseDto<string> SendDefaultApiEndpointOutput()
-    {
-        return ApiResponseDto<string>.Create("Welcome to Minimal API Endpoint");
-    }
-
-    public static ApiResponseDto<string> SendDefaultApiEndpointV1Output()
-    {
-        return ApiResponseDto<string>.Create("Welcome to Minimal API Endpoint V1");
-    }
-}
-
