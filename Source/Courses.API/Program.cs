28e7ecc518028ef8a590555995a7bf5eec716583
Viswanatha Swamy <vishipayyallore@yahoo.com>
2023-03-10 21:56:40 +0530

    Check in - 5

--- Source/Courses.API/Program.cs
+++ Source/Courses.API/Program.cs
@@ -1,33 +1,11 @@
-using Courses.API.Configuration;
-using Courses.API.Endpoints;
-using Courses.API.Persistence;
-using Microsoft.EntityFrameworkCore;
+using Courses.API.Extensions;
 
 var builder = WebApplication.CreateBuilder(args);
 
-_ = builder.Services.AddDbContext<SchoolDbContext>(options => options.UseInMemoryDatabase(InMemoryDatabase.Name));
-
-_ = builder.Services.AddAutoMapper(typeof(AutoMapperConfig));
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-_ = builder.Services.AddEndpointsApiExplorer();
-_ = builder.Services.AddSwaggerGen();
+_ = builder.Services.ConfigureDependedServices();
 
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-
-    // TODO: To be removed once we have .sqlproj
-    using var scope = app.Services.CreateScope();
-    using var context = scope.ServiceProvider.GetService<SchoolDbContext>();
-    _ = (context?.Database.EnsureCreated());
-}
-
-app.MapHelloWorldEndpoints();
-app.MapUserEndpoints();
-app.MapCourseEndpoints();
+app.ConfigureHttpRequestPipeline();
 
 app.Run();
